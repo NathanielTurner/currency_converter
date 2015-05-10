5166d158508d2a5d68bf52b1aaffd6fdcbdb1c72
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-05-10 16:47:35 -0400

    Add parsing to code string so that all codes inputed will be capitalised. Add optimization.

--- Currency.rb
+++ Currency.rb
@@ -5,11 +5,11 @@ class Currency
 
   def initialize(amount, code = nil)
     if code != nil
-      @amount = amount.to_f
-      @code = code
+      @amount = amount.to_f.round(2)
+      @code = code.upcase
     else
-      symbols = {"usd" => "$","uer" => "€","gbp" => "£","crc" => "₡",
-      "krw" => "₩","ngn" => "₦","jpy" => "¥"}
+      symbols = {"USD" => "$","EUR" => "€","GBP" => "£","CRC" => "₡",
+      "KRW" => "₩","NGN" => "₦","JPY" => "¥"}
       amount.delete(' ')
       combo = amount.split('', 2)
       @amount = combo[1].to_f
@@ -51,49 +51,3 @@ class Currency
     return Currency.new(@amount * number, @code)
   end
 end
-=begin
-Currency objects:
-
-Should be created with an amount
-and a currency code
-
-Should equal another Currency
-object with the same amount and
-currency code
-def ==(other currency)
-self.amount self.code
-
-Should NOT equal another Currency
-object with different amount or
-currency code
-
-Should be able to be added to
-another Currency object with the
-same currency code
-
-Should be able to be subtracted
-by another Currency object with
-the same currency code
-
-Should raise a
-DifferentCurrencyCodeError when
-you try to add or subtract two
-Currency objects with different
-currency codes.
-
-Should be able to be multiplied
-by a Fixnum or Float and return
-a Currency object
-
-meep
-
-Currency.new should be able to
-take one argument with a currency
-symbol embedded in it, like
-"$1.20" or "€ 7.00", and figure
-out the correct currency code.
-It can also take two arguments
-like before, one being the amount
-and the other being the currency
-code.
-=end
