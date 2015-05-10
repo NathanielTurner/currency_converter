11e712bd6dea93cac9f5da2c53690b2884edf8e1
Nathaniel <Turner@Nathaniels-MacBook-Air.local>
2015-05-10 16:45:44 -0400

    Add functionality

--- CustomErrorHandling.rb
+++ CustomErrorHandling.rb
@@ -2,10 +2,8 @@ class CustomErrorHandling < StandardError
 
 rescue DifferentCurrencyCodeError
   puts "You cannot subtract two currencies with different currency codes."
-end
 
 rescue UnknownCurrencyCodeError
   puts "The currency code given is unknown to me."
-end
 
 end
